23bd5e3410e6054d82e72e35d3f267811b09ddbd
Andrey Varlamov <varlamov.and@gmail.com>
2022-11-14 19:44:23 -0800

    Tutorial > 2.1 - Add components and entities

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use rltk::{Rltk, GameState};
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
 
-struct State {}
+// Struct State - a class
+struct State {
+    ecs: World
+}
+// State struct implements a trait (i.e. an interface) and overrides the tick function
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
@@ -8,11 +15,71 @@ impl GameState for State {
     }
 }
 
+/* POD (Plain Old Data) - no logic - "pure" ECS
+   2 reasons to use this model:
+   1) keeps all the logic code in "systems" part of ECS
+   2) very fast to keep all of the positions next to each other in memory with no redirects */
+
+// Without using specs-derive:
+// struct Position {
+//     x: i32,
+//     y: i32
+// }
+
+// impl Component for Position {
+//     type Storage = VecStorage<Self>;
+// }
+
+// Using specs-derive
+// A derive macro: "from ths basic data derive the boilerplate needed for Component"
+#[derive(Component)] 
+struct Position {
+    x: i32,
+    y: i32
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
-    let gs = State{ };
+    let mut gs = State{
+        ecs: World::new()
+    };
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    // Builder pattern - common in Rust
+    // Each function returns a copy of itself (EntityByilder)
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .build();
+    }
+
     rltk::main_loop(context, gs)
 }
