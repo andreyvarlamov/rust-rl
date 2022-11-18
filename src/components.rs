5c4d0802996f80c211020f79dc772dd2109dc8b2
Andrey Varlamov <varlamov.and@gmail.com>
2022-11-17 17:16:52 -0800

    Tutorial > Ch4 - Clean up

---
+++ src/components.rs
@@ -0,0 +1,37 @@
+use specs::prelude::*;
+use specs_derive::*;
+use rltk::{ RGB };
+
+/* POD (Plain Old Data) - no logic - "pure" ECS
+   2 reasons to use this model:
+   1) keeps all the logic code in "systems" part of ECS
+   2) very fast to keep all of the positions next to each other in memory with no redirects */
+
+// Without using specs-derive:
+// struct Position {
+//     x: i32,
+//     y: i32
+// }
+
+// impl Component for Position {
+//     type Storage = VecStorage<Self>;
+// }
+
+// Using specs-derive
+// A derive macro: "from ths basic data derive the boilerplate needed for Component"
+#[derive(Component)] 
+pub struct Position {
+    pub x: i32,
+    pub y: i32
+}
+
+#[derive(Component)]
+pub struct Renderable {
+    pub glyph: rltk::FontCharType,
+    pub fg: RGB,
+    pub bg: RGB
+}
+
+#[derive(Component,Debug)]
+// Component with no data - "tag component"
+pub struct Player {}
--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,21 @@
-use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use rltk::{ GameState, Rltk, RGB };
 use specs::prelude::*;
-use std::cmp::{max, min};
-use specs_derive::Component;
 
-// ------------------- Components --------------------------------
-
-/* POD (Plain Old Data) - no logic - "pure" ECS
-   2 reasons to use this model:
-   1) keeps all the logic code in "systems" part of ECS
-   2) very fast to keep all of the positions next to each other in memory with no redirects */
-
-// Without using specs-derive:
-// struct Position {
-//     x: i32,
-//     y: i32
-// }
-
-// impl Component for Position {
-//     type Storage = VecStorage<Self>;
-// }
-
-// Using specs-derive
-// A derive macro: "from ths basic data derive the boilerplate needed for Component"
-#[derive(Component)] 
-struct Position {
-    x: i32,
-    y: i32
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType,
-    fg: RGB,
-    bg: RGB
-}
-
-#[derive(Component,Debug)]
-// Component with no data - "tag component"
-struct Player {}
-
-// ------------------- Systems --------------------------------
-
-// ------------------- Functions --------------------------------
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != TileType::Wall {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    match ctx.key {
-        None => {},
-        Some(key) => match key {
-            VirtualKeyCode::H => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::L => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::K => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
-            _ => {},
-        },
-    }
-}
-
-pub fn xy_idx(x: i32, y: i32) -> usize {
-    (y as usize * 80) + x as usize
-}
-
-fn new_map() -> Vec<TileType> {
-    /*  vec! - procedural macro 
-             - allows to define a Vec with the same syntax as an array */
-    let mut map = vec![TileType::Floor; 80*50];
-
-    // Make the boundary walls
-    for x in 0..80 {
-        map[xy_idx(x, 0)] = TileType::Wall;
-        map[xy_idx(x, 49)] = TileType::Wall;
-    }
-    for y in 0..50 {
-        map[xy_idx(0, y)] = TileType::Wall;
-        map[xy_idx(79, y)] = TileType::Wall;
-    }
-
-    // Add random walls
-    let mut rng = rltk::RandomNumberGenerator::new();
-
-    for _i in 0..400 {
-        let x = rng.roll_dice(1, 79);
-        let y = rng.roll_dice(1, 49);
-        let idx = xy_idx(x, y);
-        if idx != xy_idx(40, 25) {
-            map[idx] = TileType::Wall;
-        }
-    }
-
-    map
-}
-
-fn draw_map(map: &[TileType], ctx: &mut Rltk) {
-    let mut y = 0;
-    let mut x = 0;
-    for tile in map.iter() {
-        match tile {
-            TileType::Floor => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.5, 0.5, 0.5),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('.')
-                );
-            }
-            TileType::Wall => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.0, 1.0, 0.0),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('#')
-                );
-            }
-        }
-
-        x += 1;
-        if x > 79 {
-            x = 0;
-            y += 1;
-        }
-    }
-}
-
-// ------------------- Enums -------------------------------
-/* More derived features (from Rust itself)
-   - Clone - adds .clone() method to type, allowing a copy to be made programmatically
-   - Copy - changes the default from moving the object on assignment to making a copy
-     so tile1 = tile2 leaves both values valid and not in a "moved from" state
-   - PartialEq - allows to use == to see if two tile types match 
-     Otherwise tile_type == TileType::Wall wouldn't compile */
-#[derive(PartialEq, Copy, Clone)]
-enum TileType {
-    Wall, Floor
-}
-
-// ------------------- Main --------------------------------
+mod components;
+pub use components::*;
+mod map;
+pub use map::*;
+mod player;
+use player::*;
 // Struct State - a class
-struct State {
+pub struct State {
     ecs: World
+}
+impl State {
+    fn run_systems(&mut self) {
+
+        self.ecs.maintain();
+    }
 }
 // State struct implements a trait (i.e. an interface) and overrides the tick function
 impl GameState for State {
@@ -170,12 +34,6 @@ impl GameState for State {
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
-    }
-}
-impl State {
-    fn run_systems(&mut self) {
-
-        self.ecs.maintain();
     }
 }
 
