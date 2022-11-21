398e9d79f0c4c9a0915e76d335bed4cd393f348f
Andrey Varlamov <varlamov.and@gmail.com>
2022-11-20 19:18:07 -0800

    Tutorial > Ch6 - Monsters and Monster AI Sys

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ mod rect;
 pub use rect::Rect;
 mod visibility_system;
 use visibility_system::VisibilitySystem;
+mod monster_ai_system;
+use monster_ai_system::MonsterAI;
 
 // Consts
 const SHOW_FPS : bool = false;
@@ -25,6 +27,8 @@ impl State {
     fn run_systems(&mut self) {
         let mut vis = VisibilitySystem {};
         vis.run_now(&self.ecs);
+        let mut mob = MonsterAI {};
+        mob.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -42,9 +46,13 @@ impl GameState for State {
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
+        let map = self.ecs.fetch::<Map>();
 
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] {
+                ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            }
         }
 
         if SHOW_FPS {
@@ -76,24 +84,58 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
     gs.ecs.register::<Viewshed>();
+    gs.ecs.register::<Monster>();
 
     let map : Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
-    gs.ecs.insert(map);
 
     // Builder pattern - common in Rust
     // Each function returns a copy of itself (EntityByilder)
     gs.ecs
         .create_entity()
-        .with(Position { x: player_x, y: player_y })
+        .with(Position { x : player_x, y : player_y })
         .with(Renderable {
-            glyph: rltk::to_cp437('@'),
-            fg: RGB::named(rltk::YELLOW),
-            bg: RGB::named(rltk::BLACK),
+            glyph : rltk::to_cp437('@'),
+            fg : RGB::named(rltk::YELLOW),
+            bg : RGB::named(rltk::BLACK),
         })
         .with(Player{})
-        .with(Viewshed { visible_tiles : Vec::new(), range : 8, dirty :true })
+        .with(Viewshed {
+            visible_tiles : Vec::new(),
+            range : 8,
+            dirty : true
+        })
         .build();
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for room in map.rooms.iter().skip(1) {
+        let (x, y) = room.center();
+
+        let glyph : rltk::FontCharType;
+        let roll = rng.roll_dice(1, 2);
+        match roll {
+            1 => { glyph = rltk::to_cp437('g') }
+            _ => { glyph = rltk::to_cp437('o') }
+        }
+
+        gs.ecs
+            .create_entity()
+            .with(Position { x, y })
+            .with(Renderable {
+                glyph : glyph,
+                fg : RGB::named(rltk::RED),
+                bg : RGB::named(rltk::BLACK),
+            })
+            .with(Monster {})
+            .with(Viewshed {
+                visible_tiles : Vec::new(),
+                range : 8,
+                dirty : true
+            })
+            .build();
+    }
+
+    gs.ecs.insert(map);
 
     rltk::main_loop(context, gs)
 }
