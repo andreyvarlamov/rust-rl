4db191cce38d9e20a3292cb685b4824546ecf914
Andrey Varlamov <varlamov.and@gmail.com>
2022-11-22 06:32:02 -0800

    Tutorial > Ch7 - Pathfinding and monster chase

--- src/monster_ai_system.rs
+++ src/monster_ai_system.rs
@@ -1,31 +1,48 @@
 use specs::prelude::*;
-use super::{ Monster, Name, Viewshed };
-use rltk::{ console, Point };
+use super::{ Map, Monster, Name, Position, Viewshed };
+use rltk::{ a_star_search, console, Point };
 
 pub struct MonsterAI {}
 
 impl<'a> System<'a> for MonsterAI {
-    type SystemData = (ReadExpect<'a, Point>,
-                       ReadStorage<'a, Viewshed>,
+    #[allow(clippy::type_complexity)]
+    type SystemData = (WriteExpect<'a, Map>,
+                       ReadExpect<'a, Point>,
+                       WriteStorage<'a, Viewshed>,
                        ReadStorage<'a, Monster>,
-                       ReadStorage<'a, Name>);
+                       ReadStorage<'a, Name>,
+                       WriteStorage<'a, Position>);
 
     fn run(&mut self, data : Self::SystemData) {
-        let (player_pos, viewshed, monster, name) = data;
+        let (mut map, player_pos, mut viewshed, monster, name, mut pos) = data;
 
         for (
-            viewshed,
+            mut viewshed,
             _monster,
-            name
+            name,
+            mut pos
         ) in (
-            &viewshed,
+            &mut viewshed,
             &monster,
-            &name
+            &name,
+            &mut pos
         ).join() {
             if viewshed.visible_tiles.contains(&*player_pos) {
                 console::log(
                     &format!("{} shouts insults", name.name)
                 );
+
+                let path = a_star_search(
+                    map.xy_idx(pos.x, pos.y) as i32,
+                    map.xy_idx(player_pos.x, player_pos.y) as i32,
+                    &mut *map
+                );
+
+                if path.success && path.steps.len() > 1 {
+                    pos.x = path.steps[1] as i32 % map.width;
+                    pos.y = path.steps[1] as i32 / map.width;
+                    viewshed.dirty = true;
+                }
             }
         }
     }
