8c9573d27f431d052a8d070c7bbf38d6ab28c435
Andrey Varlamov <varlamov.and@gmail.com>
2022-11-27 11:36:45 -0800

    Tutorial > Ch8 - Finish (add scanlines)

--- src/main.rs
+++ src/main.rs
@@ -120,10 +120,12 @@ impl GameState for State {
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
+    let mut context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .with_tile_dimensions(20, 20)
         .build()?;
+    context.with_post_scanlines(true);
+
     let mut gs = State{
         ecs : World::new()
     };
