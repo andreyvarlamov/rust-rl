0bb7fa4f36a41dba7236f1d869cf66f2f1b8f908
Andrey Varlamov <varlamov.and@gmail.com>
2022-11-28 20:34:36 -0800

    Fix tooltips

--- src/gui.rs
+++ src/gui.rs
@@ -60,7 +60,9 @@ fn draw_tooltips(ecs : &World, ctx : &mut Rltk) {
     let mut tooltip : Vec<String> = Vec::new();
     for (name, position) in (&names, &positions).join() {
         let idx = map.xy_idx(position.x, position.y);
-        if position.x == mouse_pos.0 && position.y == mouse_pos.1 && map.visible_tiles[idx] {
+        if position.x == mouse_pos.0 &&
+            position.y == mouse_pos.1 &&
+            map.visible_tiles[idx] {
             tooltip.push(name.name.to_string());
         }
     }
@@ -77,27 +79,63 @@ fn draw_tooltips(ecs : &World, ctx : &mut Rltk) {
             let left_x = mouse_pos.0 - width;
             let mut y = mouse_pos.1;
             for s in tooltip.iter() {
-                ctx.print_color(left_x, y, RGB::named(rltk::BLACK), RGB::named(rltk::GREY), s);
+                ctx.print_color(
+                    left_x,
+                    y,
+                    RGB::named(rltk::BLACK),
+                    RGB::named(rltk::GREY),
+                    s
+                );
                 let padding = (width - s.len() as i32) - 1;
                 for i in 0..padding {
-                    ctx.print_color(arrow_pos.x - i, y, RGB::named(rltk::BLACK), RGB::named(rltk::GREY), &" ".to_string());
+                    ctx.print_color(
+                        arrow_pos.x - i,
+                        y,
+                        RGB::named(rltk::BLACK),
+                        RGB::named(rltk::GREY),
+                        &" ".to_string()
+                    );
                 }
                 y += 1;
             }
-            ctx.print_color(arrow_pos.x, arrow_pos.y, RGB::named(rltk::BLACK), RGB::named(rltk::GREY), "->".to_string());
+            ctx.print_color(
+                arrow_pos.x,
+                arrow_pos.y,
+                RGB::named(rltk::BLACK),
+                RGB::named(rltk::GREY),
+                "->".to_string()
+            );
         } else {
             let arrow_pos = Point::new(mouse_pos.0 + 1, mouse_pos.1);
             let left_x = mouse_pos.0 + 3;
             let mut y = mouse_pos.1;
             for s in tooltip.iter() {
-                ctx.print_color(left_x + 1, y, RGB::named(rltk::BLACK), RGB::named(rltk::GREY), s);
+                ctx.print_color(
+                    left_x + 1,
+                    y,
+                    RGB::named(rltk::BLACK),
+                    RGB::named(rltk::GREY),
+                    s
+                );
                 let padding = (width - s.len() as i32) - 1;
                 for i in 0..padding {
-                    ctx.print_color(arrow_pos.x + 1 + i, y, RGB::named(rltk::BLACK), RGB::named(rltk::GREY), s);
+                    ctx.print_color(
+                        arrow_pos.x + 1 + i,
+                        y,
+                        RGB::named(rltk::BLACK),
+                        RGB::named(rltk::GREY),
+                        &" ".to_string()
+                    );
                 }
                 y += 1;
             }
-            ctx.print_color(arrow_pos.x, arrow_pos.y, RGB::named(rltk::BLACK), RGB::named(rltk::GREY), &"<-".to_string());
+            ctx.print_color(
+                arrow_pos.x,
+                arrow_pos.y,
+                RGB::named(rltk::BLACK),
+                RGB::named(rltk::GREY),
+                &"<-".to_string()
+            );
         }
     }
 }
