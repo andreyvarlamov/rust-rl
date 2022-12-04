7be31fa9a2bad7af48f757b83413fc97ad05c70e
Andrey Varlamov <varlamov.and@gmail.com>
2022-12-04 11:21:16 -0800

    Tutorial > Ch14 - Spawn daggers and shields

--- src/spawner.rs
+++ src/spawner.rs
@@ -157,6 +157,37 @@ fn confusion_scroll(ecs : &mut World, x : i32, y : i32) {
         .build();
 }
 
+fn dagger(ecs : &mut World, x : i32, y : i32) {
+    ecs.create_entity()
+        .with(Position { x, y })
+        .with(Renderable {
+            glyph : rltk::to_cp437('/'),
+            fg : RGB::named(rltk::SLATEGRAY),
+            bg : RGB::named(rltk::CYAN),
+            render_order : 2
+        })
+        .with(Name{ name : "Dagger".to_string() })
+        .with(Item{})
+        .marked::<SimpleMarker<SerializeMe>>()
+        .build();
+}
+
+fn shield(ecs : &mut World, x : i32, y : i32) {
+    ecs.create_entity()
+        .with(Position { x, y })
+        .with(Renderable {
+            glyph : rltk::to_cp437('('),
+            fg : RGB::named(rltk::BROWN2),
+            bg : RGB::named(rltk::CYAN),
+            render_order : 2
+        })
+        .with(Name{ name : "Shield".to_string() })
+        .with(Item{})
+        .marked::<SimpleMarker<SerializeMe>>()
+        .build();
+}
+
+
 fn room_table(map_depth : i32) -> RandomTable {
     return RandomTable::new()
         .add("Goblin", 10)
@@ -164,7 +195,9 @@ fn room_table(map_depth : i32) -> RandomTable {
         .add("Health Potion", 7)
         .add("Fireball Scroll", 2 + map_depth)
         .add("Confusion Scroll", 2 + map_depth)
-        .add("Magic Missile Scroll", 4);
+        .add("Magic Missile Scroll", 4)
+        .add("Dagger", 10)
+        .add("Shield", 10);
 }
 
 /// Fill room with stuff
@@ -207,6 +240,8 @@ pub fn spawn_room(ecs : &mut World, room : &Rect, map_depth : i32) {
             "Fireball Scroll" => fireball_scroll(ecs, x, y),
             "Confusion Scroll" => confusion_scroll(ecs, x, y),
             "Magic Missile Scroll" => magic_missile_scroll(ecs, x, y),
+            "Dagger" => dagger(ecs, x, y),
+            "Shield" => shield(ecs, x, y),
             _ => {}
         }
     }
